6ebe87d1cb25e365ef8a1bfa4647392385cdec0d
Oleg <oleg@MacBook-Air-Oleg.local>
2024-08-10 16:13:55 +0300

    added delete/update article

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,119 +1,113 @@
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
-import { validationResult } from 'express-validator'
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
-import UserModel from '../models/User.js'
+import UserModel from "../models/User.js";
 
 export const register = async (req, res) => {
-    try {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json(errors.array())
-        }
+  try {
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
 
-        const password = req.body.password;
-        const salt = await bcrypt.genSalt(10);
-        const hash = await bcrypt.hash(password, salt)
+    const doc = new UserModel({
+      email: req.body.email,
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatarUrl,
+      passwordHash: hash,
+    });
 
-        const doc = new UserModel({
-            email: req.body.email,
-            fullName: req.body.fullName,
-            avatarUrl: req.body.avatarUrl,
-            passwordHash: hash,
-        });
+    const user = await doc.save();
 
-        const user = await doc.save();
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      "secret123",
+      {
+        expiresIn: "30d",
+      }
+    );
 
-        const token = jwt.sign(
-            {
-                _id: user._id,
-            },
-            'secret123',
-            {
-                expiresIn: '30d',
-            }
-        )
+    const { passwordHash, ...userData } = user._doc;
 
-        const { passwordHash, ...userData } = user._doc
-
-        res.json({
-            ...userData,
-            token,
-        }
-        )
-
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({
-            message: 'Error during registration',
-        })
-    }
-}
+    res.json({
+      ...userData,
+      token,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Error during registration",
+    });
+  }
+};
 
 export const login = async (req, res) => {
-    try {
-        const user = await UserModel.findOne({ email: req.body.email });
+  try {
+    const user = await UserModel.findOne({ email: req.body.email });
 
-        if (!user) {
-            return res.status(404).json({
-                message: 'Such a user does not exist',
-            });
-        }
-
-        const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash);
-
-        if (!isValidPass) {
-            return res.status(400).json({
-                message: 'incorrect login or password',
-            })
-        }
-
-        const token = jwt.sign(
-            {
-                _id: user._id,
-            },
-            'secret123',
-            {
-                expiresIn: '30d',
-            }
-        );
-
-        const { passwordHash, ...userData } = user._doc
-
-        res.json({
-            ...userData,
-            token,
-        });
-
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({
-            message: 'Error during login',
-        })
+    if (!user) {
+      return res.status(404).json({
+        message: "Such a user does not exist",
+      });
     }
-}
+
+    const isValidPass = await bcrypt.compare(
+      req.body.password,
+      user._doc.passwordHash
+    );
+
+    if (!isValidPass) {
+      return res.status(400).json({
+        message: "incorrect login or password",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      "secret123",
+      {
+        expiresIn: "30d",
+      }
+    );
+
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json({
+      ...userData,
+      token,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Error during login",
+    });
+  }
+};
 
 export const getMe = async (req, res) => {
-    try {
-        const user = await UserModel.findById(req.userId);
+  try {
+    const user = await UserModel.findById(req.userId);
 
-        if (!user) {
-            return res.status(404).json({
-                message: "No user found"
-            })
-        }
-
-        const { passwordHash, ...userData } = user._doc
-
-        res.json({ userData });
-
-        res.json({
-            success: true
-        })
-    } catch (err) {
-        console.log(err);
-        res.status(404).json({
-            message: 'Access denied',
-        })
+    if (!user) {
+      return res.status(404).json({
+        message: "No user found",
+      });
     }
-}
+
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json({ userData });
+
+    res.json({
+      success: true,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(404).json({
+      message: "Access denied",
+    });
+  }
+};
--- index.js
+++ index.js
@@ -1,35 +1,83 @@
-import express from 'express';
-import mongoose from 'mongoose';
+import express from "express";
+import multer from "multer";
 
-import { registerValidation, loginValidation, postCreateValidation } from './validations.js';
+import mongoose from "mongoose";
 
-import checkAuth from './utils/checkAuth.js';
+import {
+  registerValidation,
+  loginValidation,
+  postCreateValidation,
+} from "./validations.js";
 
-import * as UserController from './controllers/UserController.js';
-import * as PostController from './controllers/PostController.js';
+import { handleValidationErrors, checkAuth } from "./utils/index.js";
 
-mongoose.connect('mongodb+srv://admin:wwwwww@cluster0.val78.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0')
-    .then(() => console.log('DB ok'))
-    .catch((err) => console.log('DB error', err));
+import { UserController, PostController } from "./controllers/index.js";
 
-const app = express()
+mongoose
+  .connect(
+    "mongodb+srv://admin:wwwwww@cluster0.val78.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => console.log("DB ok"))
+  .catch((err) => console.log("DB error", err));
+
+const app = express();
+
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => {
+    cb(null, "uploads");
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
 
 app.use(express.json());
+app.use("/uploads/", express.static("uploads"));
 
-app.post('/auth/login', loginValidation, UserController.login);
-app.post('/auth/register', registerValidation, UserController.register)
-app.get('/auth/me', checkAuth, UserController.getMe);
+app.post(
+  "/auth/login",
+  loginValidation,
+  handleValidationErrors,
+  UserController.login
+);
+app.post(
+  "/auth/register",
+  registerValidation,
+  handleValidationErrors,
+  UserController.register
+);
+app.get("/auth/me", checkAuth, UserController.getMe);
 
-app.get('/posts', PostController.getAll);
-app.get('/posts/:id', PostController.getOne);
-app.post('/posts', checkAuth, postCreateValidation, PostController.create);
-// app.delete('/posts', PostController.remove);
-// app.patch('/posts', PostController.update);
+app.post("/upload", checkAuth, upload.single("image"), (req, res) => {
+  res.json({
+    url: `/uploads/${req.file.originalname}`,
+  });
+});
+
+app.get("/posts", PostController.getAll);
+app.get("/posts/:id", PostController.getOne);
+app.post(
+  "/posts",
+  checkAuth,
+  postCreateValidation,
+  handleValidationErrors,
+  PostController.create
+);
+app.delete("/posts/:id", checkAuth, PostController.remove);
+app.patch(
+  "/posts/:id",
+  checkAuth,
+  postCreateValidation,
+  handleValidationErrors,
+  PostController.update
+);
 
 app.listen(4444, (err) => {
-    if (err) {
-        return console.log(err);
-    }
+  if (err) {
+    return console.log(err);
+  }
 
-    console.log('Server running')
-})
+  console.log("Server running");
+});
